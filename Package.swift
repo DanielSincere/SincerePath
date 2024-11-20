816defdb39f95298c5062402b8b149dcd6c2ba70
Daniel Sincere <daniel@danielsincere.com>
2024-11-20 17:55:31 -0500

    iOS 13

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "SincerePath",
   platforms: [
-    .macOS(.v11), .iOS(.v14)
+    .macOS(.v11), .iOS(.v13)
   ],
   products: [
     .library(name: "SincerePath", targets: ["SincerePath"]),
